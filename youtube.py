5e6030235a785a45de4bbb4ba6ebaf509d8aa387
eamon01 <75913022+eamonwong@users.noreply.github.com>
2023-02-01 18:52:33 +0000

    Create youtube.py

---
+++ youtube.py
@@ -0,0 +1,14 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title: ", yt.title)
+
+print("Views: ", yt.views)
+
+yd = yt.streams.get_highest_resolution()
+
+# ADD FOLDER HERE
+yd.download('./YTfolder')
